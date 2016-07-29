17278e2a27954efdee544f414f614567d00f3fb6
Benoit Deglane <bdeglane@users.noreply.github.com>
2016-07-29 09:27:01 +0200

    add local storage

--- app/reducers/contact.js
+++ app/reducers/contact.js
@@ -1,40 +1,59 @@
 import * as actions from '../constants/contact';
 import {hashHistory} from 'react-router';
 
+const getState = () => {
+	console.log('here');
+	let baseState = localStorage.getItem('contactApp');
+	if (baseState != null) {
+		return JSON.parse(baseState);
+	} else {
+		localStorage.setItem('contactApp', JSON.stringify(initialState));
+		return initialState;
+	}
+};
+
+
 const initialState = {
 	contacts: [
 		{
 			id: 1,
 			nom: 'Toto',
-			prenom: 'Tata'
+			prenom: 'Tata',
+			img: ''
 		}, {
 			id: 2,
 			nom: 'Tati',
-			prenom: 'Tato'
+			prenom: 'Tato',
+			img: ''
 		}, {
 			id: 3,
 			nom: 'Tetu',
-			prenom: 'Tute'
+			prenom: 'Tute',
+			img: ''
 		}
 	]
 };
+
 
 /**
  *
  * @param state
  * @param action
  */
-export const contact = (state = initialState, action) => {
+export const contact = (state = getState(), action) => {
 	switch (action.type) {
 		case actions.ADD_CONTACT:
 			state.contacts.push(action.param);
+			localStorage.setItem('contactApp', JSON.stringify(state));
 			hashHistory.push('/list');
 			return state;
 			break;
 		case actions.REMOVE_CONTACT:
 			let contact = state.contacts.indexOf(action.param);
-			if (contact != 'undefined')
+			if (contact != 'undefined') {
 				state.contacts.splice(contact, 1);
+				localStorage.setItem('contactApp', JSON.stringify(state));
+			}
 			return state;
 			break;
 		default:
